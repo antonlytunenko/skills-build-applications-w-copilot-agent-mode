16ee51e42971f7876cb6e285ea6dccdfc520eb1e
Anton Lytunenko <antonlit@gmail.com>
2026-04-18 08:46:22 +0000

    Update settings and URLs to support Codespace deployment and local development

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -27,8 +27,13 @@ DEBUG = True
 
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow localhost and Codespace public URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 # Use custom user model
 AUTH_USER_MODEL = 'octofit_tracker.User'
